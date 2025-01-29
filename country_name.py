f7e5bec275318f7c79a2a5b1c8bc8f480c54e18c
Physicus <physicus@bk.ru>
2025-01-29 15:58:58 +0300

    next step

---
+++ country_name.py
@@ -0,0 +1,90 @@
+country_name_rus = {'Afganistan': 'Афганистан', 'Algeria': 'Алжир', 'Argentina': 'Аргентина', 'Bangladesh': 'Бангладеш',
+                      'Benin': 'Бенин', 'Boliviya': 'Боливия', 'Braziliya': 'Бразилия', 'Burkina_Faso': 'Буркина-Фасо',
+                      'Burundi': 'Бурунди', 'CAR': 'Центрально-Африканская Республика', 'Chad': 'Чад', 'Chili': 'Чили',
+                      'China': 'КНР', 'DRK_Congo': 'ДРК (Конго)', 'Egipet': 'Египет', 'Ethiopia': 'Эфиопия ', 'Gabon': 'Габон',
+                      'Gvineya': 'Гвинея', 'India': 'Индия', 'Indoneziya': 'Индонезия', 'Iordaniya': 'Иордания', 'Irak': 'Ирак',
+                      'Iran': 'Иран', 'KNDR': 'КНДР', 'Kambodzha': 'Камбоджа', 'Kamerun': 'Камерун', 'Keniya': 'Кения', 'Kuba': 'Куба',
+                      'Laos': 'Лаос', 'Libya': 'Ливия', 'Madagaskar': 'Мадагаскар', 'Mali': 'Мали', 'Mexico': 'Мексика',
+                      'Mongoliya': 'Монголия', 'Morocco': 'Морокко', 'Mozambik': 'Мозамбик', 'Niger': 'Нигер', 'Oae': 'ОАЭ', 'Pakistan': 'Пакистан',
+                      'RK_Congo': 'Республика Конго', 'Respublika-koreya': 'Южная Корея', 'Ruanda': 'Руанда', 'Saudovskaya_araviya': 'Саудовская Аравия',
+                      'Senegal': 'Сенегал', 'Sudan': 'Судан', 'Syria': 'Сирия', 'Tailand': 'Таиланд', 'Tunis': 'Тунис', 'Turkey': 'Турция',
+                      'Urugvay': 'Уругвай', 'Venesuela': 'Венесуэла', 'Yaponiya': 'Япония', 'Yemen': 'Йемен', 'Yuar': 'ЮАР',
+                      'Zimbabve': 'Зимбабве', 'angola': 'Ангола', 'belorussiya': 'Беларусь', 'botsvana': 'Ботсвана',
+                      'ekvatorialnaya-gvineya': 'Экваториальная Гвинея', 'franciya': 'Франция', 'gana': 'Гана', 'germaniya': 'Германия',
+                      'ispaniya': 'Испания', 'italiya': 'Италия', 'izrail': 'Израиль', 'kolumbiya': 'Колумбия', 'kot-d-ivuar': "Кот-д'Ивуар",
+                      'litva': 'Литва', 'livan': 'Ливан', 'namibiya': 'Намибия', 'nigeriya': 'Нигерия', 'oman': 'Оман', 'palestina': 'Палестина',
+                      'paragvay': 'Парагвай', 'peru': 'Перу', 'portugaliya': 'Португалия', 'ssha': 'США', 'tanzaniya': 'Танзания',
+                      'tayvan': 'КНР (Тайвань)', 'uganda': 'Уганда', 'vengriya': 'Венгрия', 'zambiya': 'Замбия', 'vetnam': 'Вьетнам',
+                    'myanma': 'Мьянма'}
+
+
+country_name_rus_T = {'Алжир': 'Algeria', 'Ангола': 'angola', 'Аргентина': 'Argentina', 'Афганистан': 'Afganistan', 'Бангладеш': 'Bangladesh',
+                      'Беларусь': 'belorussiya', 'Бенин': 'Benin', 'Боливия': 'Boliviya', 'Ботсвана': 'botsvana', 'Бразилия': 'Braziliya',
+                      'Буркина-Фасо': 'Burkina_Faso', 'Бурунди': 'Burundi', 'Венгрия': 'vengriya', 'Венесуэла': 'Venesuela', 'Вьетнам': 'vetnam',
+                      'Габон': 'Gabon', 'Гана': 'gana', 'Гвинея': 'Gvineya', 'Германия': 'germaniya', 'ДРК (Конго)': 'DRK_Congo', 'Египет': 'Egipet',
+                      'Замбия': 'zambiya', 'Зимбабве': 'Zimbabve', 'Израиль': 'izrail', 'Индия': 'India', 'Индонезия': 'Indoneziya', 'Иордания': 'Iordaniya',
+                      'Ирак': 'Irak', 'Иран': 'Iran', 'Испания': 'ispaniya', 'Италия': 'italiya', 'Йемен': 'Yemen', 'КНДР': 'KNDR', 'КНР': 'China',
+                      'КНР (Тайвань)': 'tayvan', 'Камбоджа': 'Kambodzha', 'Камерун': 'Kamerun', 'Кения': 'Keniya', 'Колумбия': 'kolumbiya', "Кот-д'Ивуар": 'kot-d-ivuar',
+                      'Куба': 'Kuba', 'Лаос': 'Laos', 'Ливан': 'livan', 'Ливия': 'Libya', 'Литва': 'litva', 'Мадагаскар': 'Madagaskar', 'Мали': 'Mali',
+                      'Мексика': 'Mexico', 'Мозамбик': 'Mozambik', 'Монголия': 'Mongoliya', 'Морокко': 'Morocco', 'Мьянма': 'myanma', 'Намибия': 'namibiya',
+                      'Нигер': 'Niger', 'Нигерия': 'nigeriya', 'ОАЭ': 'Oae', 'Оман': 'oman', 'Пакистан': 'Pakistan', 'Палестина': 'palestina', 'Парагвай': 'paragvay',
+                      'Перу': 'peru', 'Португалия': 'portugaliya', 'Республика Конго': 'RK_Congo', 'Руанда': 'Ruanda', 'США': 'ssha',
+                      'Саудовская Аравия': 'Saudovskaya_araviya', 'Сенегал': 'Senegal', 'Сирия': 'Syria', 'Судан': 'Sudan', 'Таиланд': 'Tailand',
+                      'Танзания': 'tanzaniya', 'Тунис': 'Tunis', 'Турция': 'Turkey', 'Уганда': 'uganda', 'Уругвай': 'Urugvay', 'Франция': 'franciya',
+                      'Центрально-Африканская Республика': 'CAR', 'Чад': 'Chad', 'Чили': 'Chili', 'Экваториальная Гвинея': 'ekvatorialnaya-gvineya',
+                      'Эфиопия ': 'Ethiopia', 'ЮАР': 'Yuar', 'Южная Корея': 'Respublika-koreya', 'Япония': 'Yaponiya'}
+
+country_links_BRE = {'Алжир': 'https://old.bigenc.ru/geography/text/3260130', 'Ангола': 'https://old.bigenc.ru/geography/text/4013841',
+                     'Аргентина': 'https://old.bigenc.ru/geography/text/3252173', 'Афганистан': 'https://old.bigenc.ru/geography/text/4053221',
+                     'Бангладеш': 'https://old.bigenc.ru/geography/text/4067019', 'Беларусь': 'https://bigenc.ru/c/respublika-belarus-a2c1c6',
+                     'Бенин': 'https://old.bigenc.ru/geography/text/4014617', 'Боливия': 'boliviya', 'Ботсвана': 'botsvana',
+                     'Бразилия': 'https://bigenc.ru/c/braziliia-e3108d', 'Буркина-Фасо': 'https://bigenc.ru/c/burkina-faso-8b7873',
+                     'Бурунди': 'https://old.bigenc.ru/geography/text/5709667', 'Венгрия': 'https://bigenc.ru/c/vengriia-93b24c',
+                     'Венесуэла': 'https://old.bigenc.ru/geography/text/1907148', 'Вьетнам': 'https://old.bigenc.ru/geography/text/4357956',
+                     'Габон': 'https://old.bigenc.ru/geography/text/5709887', 'Гвинея': 'https://bigenc.ru/c/gvineia-816e89',
+                     'Германия': 'https://bigenc.ru/c/germaniia-21a63a', 'ДРК (Конго)': 'https://bigenc.ru/c/demokraticheskaia-respublika-kongo-db10f5',
+                     'Египет': 'https://bigenc.ru/c/egipet-40be3b', 'Зимбабве': 'https://bigenc.ru/c/zimbabve-ce737b',
+                     'Израиль': 'https://old.bigenc.ru/geography/text/2003352', 'Индия': 'https://bigenc.ru/c/indiia-47b8bb',
+                     'Индонезия': 'https://bigenc.ru/c/indoneziia-92953b', 'Иордания': 'https://old.bigenc.ru/geography/text/2018403',
+                     'Ирак': 'https://old.bigenc.ru/geography/text/2019253', 'Иран': 'https://old.bigenc.ru/geography/text/2019712',
+                     'Испания': 'https://old.bigenc.ru/geography/text/2632850', 'Италия': 'https://bigenc.ru/c/italiia-02fc0e',
+                     'Йемен': 'https://old.bigenc.ru/geography/text/2028825', 'КНДР': 'https://old.bigenc.ru/geography/text/2096962',
+                     'КНР': 'https://old.bigenc.ru/geography/text/2068564', 'Камбоджа': 'https://old.bigenc.ru/geography/text/2037904',
+                     'Камерун': 'https://old.bigenc.ru/geography/text/2038869', 'Кения': 'https://old.bigenc.ru/geography/text/2060435',
+                     'Куба': 'https://bigenc.ru/c/kuba-f59d2b', 'Лаос': 'https://old.bigenc.ru/geography/text/2132727', 'Ливан': 'https://old.bigenc.ru/geography/text/2649796',
+                     'Ливия': 'https://old.bigenc.ru/geography/text/2172885', 'Литва': 'https://old.bigenc.ru/geography/text/2175974',
+                     'Мадагаскар': 'https://bigenc.ru/c/madagaskar-3bcf24', 'Мали': 'https://old.bigenc.ru/geography/text/2169150',
+                     'Мексика': 'https://bigenc.ru/c/meksika-c3e1d2', 'Мозамбик': 'https://old.bigenc.ru/geography/text/2222502',
+                     'Монголия': 'https://bigenc.ru/c/mongoliia-aed287', 'Марокко': 'https://old.bigenc.ru/geography/text/2187692',
+                     'Нигер': 'https://old.bigenc.ru/geography/text/2265040', 'ОАЭ': 'https://old.bigenc.ru/geography/text/2286303',
+                     'Оман': 'https://old.bigenc.ru/geography/text/2688387', 'Пакистан': 'https://old.bigenc.ru/geography/text/2313092',
+                     'Парагвай': 'https://old.bigenc.ru/geography/text/2706259', 'Перу': 'https://old.bigenc.ru/geography/text/2775845',
+                     'Португалия': 'https://old.bigenc.ru/geography/text/5741395', 'Республика Конго': 'https://bigenc.ru/c/respublika-kongo-0f8861',
+                     'Руанда': 'https://old.bigenc.ru/geography/text/3517411', 'США': 'https://bigenc.ru/c/soedinionnye-shtaty-ameriki-640c9b',
+                     'Саудовская Аравия': 'https://old.bigenc.ru/geography/text/6030911', 'Сенегал': 'https://old.bigenc.ru/geography/text/3548336',
+                     'Сирия': 'https://old.bigenc.ru/geography/text/3665888', 'Судан': 'https://bigenc.ru/c/sudan-619bdc',
+                     'Таиланд': 'https://old.bigenc.ru/geography/text/4178979', 'Танзания': 'https://bigenc.ru/c/tanzaniia-d04ba3',
+                     'Тунис': 'https://bigenc.ru/c/tunis-eecae4', 'Турция': 'https://bigenc.ru/c/turtsiia-e46b44', 'Уругвай': 'https://old.bigenc.ru/geography/text/4701529',
+                     'Франция': 'https://bigenc.ru/c/frantsiia-370c99',  'Чад': 'https://old.bigenc.ru/geography/text/6025186', 'Чили': 'https://old.bigenc.ru/geography/text/4686220',
+                     'Эфиопия ': 'https://bigenc.ru/c/efiopiia-021ef7', 'ЮАР': 'https://bigenc.ru/c/iuzhno-afrikanskaia-respublika-133acf',
+                     'Южная Корея': 'https://bigenc.ru/c/respublika-koreia-f9a118', 'Япония': 'https://old.bigenc.ru/geography/text/4919294',
+                     'Таджикистан': 'https://old.bigenc.ru/geography/text/4178539', 'Экваториальная Гвинея':'https://old.bigenc.ru/geography/text/4941066',
+                     'Гана': 'https://old.bigenc.ru/geography/text/2343531', 'Колумбия': 'https://old.bigenc.ru/geography/text/2082520',
+                     "Кот-д'Ивуар": 'https://old.bigenc.ru/geography/text/2103312', 'Намибия': 'https://old.bigenc.ru/geography/text/2247323',
+                     'Нигерия': 'https://bigenc.ru/c/nigeriia-3e8a7f', 'Палестина': 'https://bigenc.ru/c/gosudarstvo-palestina-164a94',
+                     'Уганда': 'https://old.bigenc.ru/geography/text/4214563', 'Замбия': 'https://bigenc.ru/c/zambiia-9dfc3a',
+                     'Вьетнам': 'https://old.bigenc.ru/geography/text/4357956', 'Мьянма': 'https://old.bigenc.ru/geography/text/2241273'}
+
+
+if __name__ == "__main__":
+    country_name_rus_T = {}
+    for item in country_name_rus.keys():
+        country_name_rus_T[country_name_rus[item]] = item
+
+    print({key: val for key, val in sorted(country_name_rus.items(), key=lambda ele: ele[0])})
+    print ({key: val for key, val in sorted(country_name_rus_T.items(), key = lambda ele: ele[0])})
+    print (country_name_rus_T.keys())
+
+    for item in country_name_rus_T.keys():
+        if item not in country_links_BRE.keys():
+            print(item)
